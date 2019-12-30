2c10e2ec178bae25126af6a9a93adaab24732cde
王莉萍 <liping.wang@bjyada.com>
2019-12-30 13:28:24 +0800

    wangliping

--- static/js/staticMerchat.js
+++ static/js/staticMerchat.js
@@ -1,8 +1,22 @@
-function trId() {
+function trId () {
   //添加序号
   var listId = 0;
   $("#lplist-table").find("tbody").first().children("tr").each(function () {
     listId++;
     $(this).children("td").eq(0).text(listId);
   });
+}
+
+
+//二级菜单中的新增和编辑页面中的，所属类型切换显示不同的内容
+function belongTypeFun () {
+  var menuType = $("#menuType").val();
+  $(".belongType").hide();
+  if (menuType === "自定义") {
+    $(".belongType1").show();
+  } else if (menuType === "列表") {
+    $(".belongType2").show();
+  } else if (menuType === "富文本框") {
+    $(".belongType3").show();
+  }
 }
